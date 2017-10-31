a776a1e880e9dae3d8681f675239808f5f16ed46
A Q Khan <aqk.neo@gmail.om>
2017-10-31 16:10:22 +0500

    Init

---
+++ src/components/common/index.js
@@ -0,0 +1,6 @@
+// Contains references of other common components. Allows easy destructed imports in other imports
+
+export * from './Button';
+export * from './Card';
+export * from './CardSection';
+export * from './Header';
